07c12ae473183210da754e489da875a79955b47e
safi <safiazim8@gmail.com>
2020-11-02 22:00:42 +0500

    Applying Basic styles

--- App.js
+++ App.js
@@ -1,27 +1,23 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
-  // const [outputText, setOutputText] = useState(
-  //   "Open up App.js to start working on your app!"
-  // );
+  
   return (
-    <View style={styles.container}>
-      <View>..</View>
+    <View style={{padding: 50}}>
       <View>
-        
+        <TextInput 
+         placeholder="Course Goal"
+         style={{borderColor:"black", borderWidth: 1, padding:10}}/>
+        <Button title="ADD"/>
       </View>
-      <StatusBar style="auto" />
+      <View>
+
+      </View>
+   
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const styles = StyleSheet.create({});
