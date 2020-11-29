4208fd19e026ec02de6510d35381854de1a15ceb
safi <safiazim8@gmail.com>
2020-11-29 16:12:25 +0500

    Closing Modal

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ export default function App() {
       ...currentGoals,
       { id: Math.random().toString(), value: goalTitle },
     ]);
+    setIsAddMode(false);
   };
 
   const removeGoalHandler = (goalId) => {
